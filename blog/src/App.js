687e2731f19d4154d6e9de13fc8e79ed0e24ea0a
danphne <dancerelrin@gmail.com>
2022-12-05 19:38:18 +0900

    react setup & basic

---
+++ blog/src/App.js
@@ -0,0 +1,24 @@
+import logo from './logo.svg';
+import './App.css';
+
+function App() {
+
+  let post = '전주 삼겹살 맛집';
+
+  return (
+    <div className="App">
+      
+      <div className="black-nav">
+        <h4>블로그임</h4>
+      </div>
+      <div className='list'>
+        <h4>글제목</h4>
+        <p>2월 17일 발행</p>
+      </div>
+      
+
+    </div>
+  );
+}
+
+export default App;
