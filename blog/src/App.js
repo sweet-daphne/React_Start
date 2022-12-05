261d4fc7ee5a3a214426f17c9f35b7952e296933
danphne <dancerelrin@gmail.com>
2022-12-05 20:00:06 +0900

    react state

--- blog/src/App.js
+++ blog/src/App.js
@@ -1,22 +1,32 @@
 import logo from './logo.svg';
 import './App.css';
+import { useState } from 'react';
 
 function App() {
 
   let post = '전주 삼겹살 맛집';
+  let [title, a] = useState(['남자 화장품 추천','전주 맛집','React 장인']);
+  
 
   return (
     <div className="App">
       
       <div className="black-nav">
-        <h4>블로그임</h4>
+        <h4>ReactBlog</h4>
       </div>
       <div className='list'>
-        <h4>글제목</h4>
+        <h4>{title[0]}</h4>
+        <p>2월 17일 발행</p>
+      </div>
+      <div className='list'>
+        <h4>{title[1]}</h4>
+        <p>2월 17일 발행</p>
+      </div>
+      <div className='list'>
+        <h4>{title[2]}</h4>
         <p>2월 17일 발행</p>
       </div>
       
-
     </div>
   );
 }
